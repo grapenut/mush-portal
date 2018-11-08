6c7f496b86a1802c15642f301218c1a0e82e40a7
Jeremy Ritter <jeremy.s.ritter@outlook.com>
2018-11-07 19:53:05 -0500

    updated to new react version

---
+++ build/precache-manifest.dd77113eaf6becbb178e3a0d89330940.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "163fc1e5ae28113169ac",
+    "url": "/static/js/main.163fc1e5.chunk.js"
+  },
+  {
+    "revision": "4012d46c5e6c981dc668",
+    "url": "/static/js/1.4012d46c.chunk.js"
+  },
+  {
+    "revision": "163fc1e5ae28113169ac",
+    "url": "/static/css/main.e3ffe8a2.chunk.css"
+  },
+  {
+    "revision": "4012d46c5e6c981dc668",
+    "url": "/static/css/1.6672db8a.chunk.css"
+  },
+  {
+    "revision": "2fc0a2b6be973fc156968e96f1a1ba1d",
+    "url": "/index.html"
+  }
+];
