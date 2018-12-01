b096c18879708e3db30ed2798e4ffc4ed32ede69
Travis CI <travis@travis-ci.org>
2018-12-01 12:29:10 +0000

    Automatic build [skip travis]

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ead740cd952050e755753a01dc2b00e4.js"
+  "./precache-manifest.716a913febf25ff836fbeec500c6504c.js"
 );
 
 workbox.clientsClaim();
