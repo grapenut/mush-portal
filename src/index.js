50fc5a2b7c1ef4f8d8f2d729f8f1f463cc61954e
grapenut <jeremy.s.ritter@outlook.com>
2018-12-02 21:50:08 -0500

    run local() function if present, load ./local.js if not, and finally fallback to mushportal.com/remote.js

--- src/index.js
+++ src/index.js
@@ -7,8 +7,13 @@ if (window.local && typeof(window.local) === "function") {
   // we defined a local() function, use that for customization
   window.local(client);
 } else {
-  // load default local customization file
-  client.loadScript('./local.js');
+  try {
+    // load default local customization file
+    client.loadScript('./local.js');
+  } catch (e) {
+    // as a last resort load remote.js from the mushportal website
+    client.loadScript('http://www.mushportal.com/remote.js');
+  }
 }
 
 client.execUserScript('onLoad.js');
