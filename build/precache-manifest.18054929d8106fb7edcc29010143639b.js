c592e356a0a76a09a29163d0cec244a488991905
Travis CI <travis@travis-ci.org>
2018-12-03 10:02:40 +0000

    Automatic build [skip travis]

---
+++ build/precache-manifest.18054929d8106fb7edcc29010143639b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f903d05442fe6577f3cd",
+    "url": "./static/js/main.f903d054.chunk.js"
+  },
+  {
+    "revision": "8265869541e3ff17a58c",
+    "url": "./static/css/1.b8619a2d.chunk.css"
+  },
+  {
+    "revision": "8265869541e3ff17a58c",
+    "url": "./static/js/1.82658695.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "d49bb9119af55f51391bb03cf36314e8",
+    "url": "./index.html"
+  }
+];
