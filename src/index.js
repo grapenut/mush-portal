aee8baefaad1a8ee2648a546ed23a027d11929b1
grapenut <jeremy.s.ritter@outlook.com>
2018-12-02 22:26:49 -0500

    fix index

--- src/index.js
+++ src/index.js
@@ -7,11 +7,10 @@ if (window.local && typeof(window.local) === "function") {
   // we defined a local() function, use that for customization
   window.local(client);
 } else {
-    client.loadScript('./local.js', null, () => {
-      // error loading local.js, try remote.js instead
-      client.loadScript('https://mushportal.com/remote.js');
-    });
-  }
+  client.loadScript('./local.js', null, () => {
+    // error loading local.js, try remote.js instead
+    client.loadScript('https://mushportal.com/remote.js');
+  });
 }
 
 client.execUserScript('onLoad.js');
