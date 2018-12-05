928ceb42bffe20a142db146d3457a949fc844f32
Travis CI <travis@travis-ci.org>
2018-12-05 06:25:19 +0000

    Automatic build [skip travis]

---
+++ build/precache-manifest.350c3966a714dc080e68ba72c1c85cb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e765cf3f31f053be31ff",
+    "url": "./static/js/main.e765cf3f.chunk.js"
+  },
+  {
+    "revision": "05cb78750b9ce9adf1bf",
+    "url": "./static/css/1.7cc32cc9.chunk.css"
+  },
+  {
+    "revision": "05cb78750b9ce9adf1bf",
+    "url": "./static/js/1.05cb7875.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "5fb62218fd603ca31da08f08b21e1f5d",
+    "url": "./index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2d6631c1146078af8de158227a0b967f.js"
+  "./precache-manifest.350c3966a714dc080e68ba72c1c85cb7.js"
 );
 
 workbox.clientsClaim();
